e435cd9822bb7a6deeb4bf6410d6823b60127404
Ben <bgoff002@plattsburgh.edu>
2021-12-13 23:46:25 -0500

    Added the API framework, started working on the Model for monster, added the configuration for the mongoDB

---
+++ MonstersApi/Configuration/MonsterConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace MonstersApi.Configuration
+{
+    public class MonsterConfiguration
+    {
+        public string MonsterCollectionName { get; set; }
+
+        public string ConnectionString { get; set; }   
+
+        public string DatabaseName { get; set; }    
+
+    }
+}
---
+++ MonstersApi/Models/Monster.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace MonstersApi.Models
+{
+    public class Monster
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+
+        // TODO: Implement classes that will allow the JSON to be read into the model
+        // Explanation I'm working off of: https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-how-to?pivots=dotnet-6-0
+
+        public string _id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<string> Habitats { get; set; }
+
+    }
+}
